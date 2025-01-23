4886f798d62580a9d429cc8024be23f8f9da636f
JefenaGracia <143041564+JefenaGracia@users.noreply.github.com>
2025-01-22 22:50:05 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -88,17 +88,6 @@ const components = {
 };
 
 
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCs-xerrIr0KpnCTihTX-GowGDAZbRZFvA",
-  authDomain: "creative-assistant-j.firebaseapp.com",
-  databaseURL: "https://creative-assistant-j-default-rtdb.firebaseio.com",
-  projectId: "creative-assistant-j",
-  storageBucket: "creative-assistant-j.firebasestorage.app",
-  messagingSenderId: "414003942125",
-  appId: "1:414003942125:web:d1400f5fa9358683f832e4",
-  measurementId: "G-NJWKCE24C4",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
